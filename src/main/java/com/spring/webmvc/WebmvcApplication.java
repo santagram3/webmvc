268d5b530ff798454ed957822f8b6b2df413117c
santagram3 <soslimso@naver.com>
2022-07-06 17:02:56 +0900

    2022.07.06 회원가입, 등등

--- src/main/java/com/spring/webmvc/WebmvcApplication.java
+++ src/main/java/com/spring/webmvc/WebmvcApplication.java
@@ -8,8 +8,8 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 @ServletComponentScan(basePackages = "com.spring.webmvc") // 서블릿을 모두 컨테이너에 등록한다.
 public class WebmvcApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebmvcApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebmvcApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/spring/webmvc/chap01/member/controller/dddd.java
@@ -0,0 +1,19 @@
+package com.spring.webmvc.chap01.member.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class dddd extends HttpServlet {
+
+  // 클래스가 extends HttpServlet 일때
+  // protected void service 이게 뜬다
+
+//
+//    @Override
+//    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.service(req, resp);
+//    }
+}
